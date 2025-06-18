8f1ef8d9ff661e30df3f918472823dca4d02c918
Cornelius Weidmann <caweidmann.dev@gmail.com>
2025-06-18 16:40:56 +0200

    fix: Update perms

--- build/build.sh
+++ build/build.sh
@@ -75,7 +75,6 @@ deploy_demo_react() {
 
 
   printf "${YELLOW}🌐 Deploying to GitHub Pages...${NC}\n"
-  # npx gh-pages -d "$DIST_DIR"
   npx gh-pages -d "$DIST_DIR" -r https://x-access-token:${GITHUB_TOKEN}@github.com/kyco/eeaas.git
 
   printf "${GREEN}✅ Deployment complete!${NC}\n"
