80b641fe79b62494e8a62501b8f8e22e73ac88e2
Cornelius Weidmann <caweidmann.dev@gmail.com>
2025-06-21 15:04:33 +0200

    style: Ensure footer is at bottom always

--- apps/docs/src/components/Layout/index.tsx
+++ apps/docs/src/components/Layout/index.tsx
@@ -1,14 +1,15 @@
+import { Box } from '@mui/material'
 import { Outlet } from 'react-router-dom'
 
 import { Footer, Header } from './components'
 
 const Layout = () => {
   return (
-    <>
+    <Box sx={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
       <Header />
       <Outlet />
       <Footer />
-    </>
+    </Box>
   )
 }
 
