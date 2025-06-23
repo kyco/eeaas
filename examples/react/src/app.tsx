e7f625316ee7c900841582032241505dd34a73e8
Cornelius Weidmann <caweidmann.dev@gmail.com>
2025-06-23 17:41:19 +0200

    feat(examples): Add react example

---
+++ examples/react/src/app.tsx
@@ -0,0 +1,40 @@
+import { useEffect } from 'react'
+
+import { initializeEeaas } from '@eeaas/core'
+
+const eeaas = initializeEeaas()
+
+eeaas.register({
+  name: 'MyFirstEgg',
+  onStart() {
+    console.log('Easter egg activated!')
+  },
+  onStop() {
+    console.log('Easter egg deactivated!')
+  },
+})
+
+function App() {
+  const start = () => {
+    eeaas.get('MyFirstEgg')?.start()
+  }
+  const stop = () => {
+    eeaas.get('MyFirstEgg')?.stop()
+  }
+
+  useEffect(() => {
+    return () => {
+      stop()
+    }
+  }, [])
+
+  return (
+    <div className="App">
+      <h1>Eeaas React Example</h1>
+      <button onClick={start}>Trigger</button>
+      <button onClick={stop}>Stop</button>
+    </div>
+  )
+}
+
+export default App
